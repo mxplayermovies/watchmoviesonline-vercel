388f3996c34b9b92a7333fee4faf2a53d185b6b2
veigasjeff <127490398+mxplayermovies@users.noreply.github.com>
2024-02-28 18:12:53 +0530

    update tracker

---
+++ js/tracker.js
@@ -0,0 +1,16 @@
+// googlescript.js
+
+(function () {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-REYHDZZ5K8';
+  
+    document.head.appendChild(script);
+  
+    script.onload = function () {
+      window.dataLayer = window.dataLayer || [];
+      function gtag() { dataLayer.push(arguments); }
+      gtag('js', new Date());
+      gtag('config', 'G-REYHDZZ5K8');
+    };
+  })();
