938764a59707f40833d8b3c74e394c5bde4f358c
veigasjeff <127490398+mxplayermovies@users.noreply.github.com>
2024-03-12 15:05:47 +0530

    Finesh it now

--- pages/movie/captain-miller-2024/index.js
+++ pages/movie/captain-miller-2024/index.js
@@ -235,50 +235,75 @@ const captain_miller_2024 = ({ movie }) => {
     "@context": "https://schema.org",
     "@graph": [
       {
-        "@type": "Article",
+        "@type": ["Person", "Organization"],
+        "@id": "https://watchmoviesonline.vercel.app/#person",
+        "name": "Dr Trailer"
+      },
+      {
+        "@type": "WebSite",
+        "@id": "https://watchmoviesonline.vercel.app/#website",
+        "url": "https://watchmoviesonline.vercel.app/",
+        "name": "Watch Movies Online™",
+        "publisher": {
+          "@id": "https://watchmoviesonline.vercel.app/#person"
+        },
+        "inLanguage": "en-US"
+      },
+      {
+        "@type": "WebPage",
+      "@id": "https://watchmoviesonline.vercel.app/movie/captain-miller-2024/#webpage",
+        "url": "https://watchmoviesonline.vercel.app/movie/captain-miller-2024/",
+        "name": `${movie.name} | Watch Movies Online™`,
+        "datePublished": "2024-01-13T13:00:00+00:00",
+        "dateModified": "2024-01-13T13:13:00+00:00",
+        "isPartOf": {
+          "@id": "https://watchmoviesonline.vercel.app/#website"
+        },
+        "inLanguage": "en-US"
+      },
+      {
+        "@type": "Person",
+        "@id": "https://watchmoviesonline.vercel.app/author/watchmoviesonline/",
+        "name": "Dr Trailer",
+        "url": "https://watchmoviesonline.vercel.app/author/watchmoviesonline/",
+        "image": {
+          "@type": "ImageObject",
+          "@id": "https://secure.gravatar.com/avatar/5d6510600085b5328aa1ab8e9a14e02a?s=96&d=mm&r=g",
+          "url": "https://secure.gravatar.com/avatar/5d6510600085b5328aa1ab8e9a14e02a?s=96&d=mm&r=g",
+          "caption": "Dr Trailer",
+          "inLanguage": "en-US"
+        },
+        "sameAs": [
+          "https://watchmoviesonline.vercel.app/"
+        ]
+      },
+      {
+        "@type": "BlogPosting",
         "headline": `Watch ${movie.name} | Watch Movies Online™`,
         "datePublished": "2024-01-13T13:00:00+00:00",
         "dateModified": "2024-01-13T13:13:00+00:00",
         "articleSection": "Movie",
         "author": {
-          "@type": "Person",
-          "@id": "https://watchmoviesonline.vercel.app/author/watchmoviesonline/",
-          "name": "Dr Trailer",
-          "url": "https://watchmoviesonline.vercel.app/author/watchmoviesonline/",
-          "image": {
-            "@type": "ImageObject",
-            "@id": "https://secure.gravatar.com/avatar/5d6510600085b5328aa1ab8e9a14e02a?s=96&d=mm&r=g",
-            "url": "https://secure.gravatar.com/avatar/5d6510600085b5328aa1ab8e9a14e02a?s=96&d=mm&r=g",
-            "caption": "Dr Trailer",
-            "inLanguage": "en-US"
-          },
-          "sameAs": [
-            "https://watchmoviesonline.vercel.app"
-          ]
+          "@id": "https://watchmoviesonline.vercel.app/author/watchmoviesonline/"
         },
         "publisher": {
-          "@type": "Organization",
-          "@id": "https://watchmoviesonline.vercel.app/#person",
-          "name": "Watch Movies Online™",
-          "logo": {
-            "@type": "ImageObject",
-            "url": "https://watchmoviesonline.vercel.app/og_image.jpg"
-          }
+          "@id": "https://watchmoviesonline.vercel.app/#person"
         },
         "description": `Watch Movies Online | ${movie.name} for free. Where you can find movies of your interest in full HD quality updated on daily basis. Watch Now or Download Now to Watch Later!`,
         "name": `Watch ${movie.name} | Watch Movies Online™`,
-        "@id": movie.url ? `${movie.url}#richSnippet` : "https://watchmoviesonline.vercel.app/#richSnippet",
+        "@id": "https://watchmoviesonline.vercel.app/movie/captain-miller-2024/#richSnippet",
         "isPartOf": {
-          "@id": movie.url ? `${movie.url}#webpage` : "https://watchmoviesonline.vercel.app/#webpage"
+          "@id": "https://watchmoviesonline.vercel.app/movie/captain-miller-2024/#webpage"
         },
         "inLanguage": "en-US",
         "mainEntityOfPage": {
-          "@id": movie.url ? `${movie.url}#webpage` : "https://watchmoviesonline.vercel.app/#webpage"
+          "@id": "https://watchmoviesonline.vercel.app/movie/captain-miller-2024/#webpage"
         }
       }
     ]
   });
   
+
   const ldJsonData = JSON.stringify({
     '@context': 'https://schema.org',
     '@type': 'Movie',
